9f05dcef6bcdaf88a21c99807a3089c6f453b226
Vera Krugliakova <vera.krugliakova@evrythng.com>
2015-06-23 19:01:41 +0300

    Batch model
    ENGINE-1363

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Batch.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Batch.java
@@ -6,13 +6,73 @@ package com.evrythng.thng.resource.model.store;
 
 import com.evrythng.thng.resource.model.core.DurableResourceModel;
 
+import java.util.Map;
+
 /**
  * Model representation for <em>batches</em>.
  */
-public abstract class Batch extends DurableResourceModel {
+public class Batch extends DurableResourceModel {
 
 	private static final long serialVersionUID = -4440334109484551488L;
-	// TODO _MS_
+
+	public enum Status {
+		EMPTY, IN_PROGRESS, COMPLETE, SEALED;
+	}
+
 	private String name;
 	private String description;
+	private Map<String, String> identifiers;
+	private Status status;
+
+	public String getName() {
+
+		return name;
+	}
+
+	public void setName(final String name) {
+
+		this.name = name;
+	}
+
+	public String getDescription() {
+
+		return description;
+	}
+
+	public void setDescription(final String description) {
+
+		this.description = description;
+	}
+
+	public Map<String, String> getIdentifiers() {
+
+		return identifiers;
+	}
+
+	public void setIdentifiers(final Map<String, String> identifiers) {
+
+		this.identifiers = identifiers;
+	}
+
+	public Status getStatus() {
+
+		return status;
+	}
+
+	public void setStatus(final Status status) {
+
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder("Batch{");
+		sb.append("name='").append(name).append('\'');
+		sb.append(", description='").append(description).append('\'');
+		sb.append(", identifiers='").append(identifiers).append('\'');
+		sb.append(", status=").append(status);
+		sb.append('}');
+		return sb.toString();
+	}
 }
