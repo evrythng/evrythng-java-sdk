e4569ab30213c168a95259cb618c297e13242192
sollecitom <michele.sollecito@evrythng.com>
2016-01-05 15:52:08 +0000

    Encapsulated fields in AcceptedResourceLocation.
    ENGINE-1924
    
    Jean still needs punishment :P

--- evrythng-java-wrapper/src/main/java/com/evrythng/java/wrapper/core/api/AcceptedResourceResponse.java
+++ evrythng-java-wrapper/src/main/java/com/evrythng/java/wrapper/core/api/AcceptedResourceResponse.java
@@ -13,16 +13,42 @@ public class AcceptedResourceResponse {
 	/**
 	 * Id of the created resource if applicable
 	 */
-	public final String resourceId;
+	private final String resourceId;
 
 	/**
 	 * Content of the Location http header if present.
 	 */
-	public final String location;
+	private final String location;
 
-	public AcceptedResourceResponse(String resourceId, String location) {
+	public AcceptedResourceResponse(final String resourceId, final String location) {
+		
 		this.resourceId = resourceId;
 		this.location = location;
 	}
 
+	/**
+	 * Id of the created resource if applicable
+	 */
+	public String getResourceId() {
+
+		return resourceId;
+	}
+
+	/**
+	 * Content of the Location http header if present.
+	 */
+	public String getLocation() {
+
+		return location;
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder("{");
+		sb.append("location='").append(location).append('\'');
+		sb.append(", resourceId='").append(resourceId).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
 }
