14e78000866452a18f8e164428fed03f45c38b7f
Aleksandr Dorogush <aleksandr.dorogush@evrythng.com>
2016-03-22 16:01:49 +0300

    Revert "Added redirectionContext field to RedirectionReaction"
    This reverts commit 531ad677dc523ccabc45963433a74daa713c20f5.

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/rule/reaction/RedirectionReaction.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/rule/reaction/RedirectionReaction.java
@@ -4,8 +4,6 @@
  */
 package com.evrythng.thng.resource.model.store.rule.reaction;
 
-import java.util.Map;
-
 /**
  * Reaction that will cause a URL redirection.
  **/
@@ -15,7 +13,6 @@ public class RedirectionReaction extends Reaction {
 	public static final String TYPE = "redirection";
 
 	private final String redirectUrl;
-	private Map<String, Object> redirectionContext;
 
 	public RedirectionReaction(final String redirectUrl) {
 
@@ -30,16 +27,6 @@ public class RedirectionReaction extends Reaction {
 
 	public String getRedirectUrl() {
 		return redirectUrl;
-	}
-
-	public Map<String, Object> getRedirectionContext() {
-
-		return redirectionContext;
-	}
-
-	public void setRedirectionContext(final Map<String, Object> redirectionContext) {
-
-		this.redirectionContext = redirectionContext;
 	}
 
 	@Override
