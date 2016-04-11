3d191fc28b1103a9115614ca7664c2cf62f053a0
sollecitom <michele.sollecito@evrythng.com>
2016-04-11 16:10:37 +0100

    Added withPassword interface.
    ENGINE-2226

---
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/security/passwords/WithPassword.java
@@ -0,0 +1,13 @@
+/*
+ * (c) Copyright 2016 EVRYTHNG Ltd London / Zurich
+ * www.evrythng.com
+ */
+
+package com.evrythng.thng.resource.model.store.security.passwords;
+
+public interface WithPassword {
+
+	String getPassword();
+
+	void setPassword(String password);
+}
