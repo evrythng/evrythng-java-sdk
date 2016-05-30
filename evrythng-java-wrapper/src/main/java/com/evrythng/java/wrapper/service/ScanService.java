cba42c7f9b6ed645b3dbd7e70e83ee9a1b250ba4
Jean Vaucher <jean@evrythng.com>
2016-05-30 14:30:44 +0200

    [ ENGINE-2454 ] Add the support for /identify endpoint in the java sdk

--- evrythng-java-wrapper/src/main/java/com/evrythng/java/wrapper/service/ScanService.java
+++ evrythng-java-wrapper/src/main/java/com/evrythng/java/wrapper/service/ScanService.java
@@ -8,6 +8,7 @@ package com.evrythng.java.wrapper.service;
 import com.evrythng.java.wrapper.ApiManager;
 import com.evrythng.java.wrapper.core.EvrythngApiBuilder.Builder;
 import com.evrythng.java.wrapper.core.EvrythngServiceBase;
+import com.evrythng.java.wrapper.core.api.QueryParamValue;
 import com.evrythng.java.wrapper.core.http.Status;
 import com.evrythng.java.wrapper.exception.EvrythngException;
 import com.evrythng.thng.resource.model.li.UrlBinding;
@@ -24,6 +25,7 @@ public class ScanService extends EvrythngServiceBase {
 	public static final String PATH_SCAN = "/scan";
 	public static final String PATH_SCAN_RECOGNIZE = PATH_SCAN + "/recognitions";
 	public static final String PATH_SCAN_BARCODE = PATH_SCAN + "/barcode";
+	public static final String PATH_IDENTIFY = "/identify";
 
 	public enum ScanMethod {
 
@@ -41,6 +43,30 @@ public class ScanService extends EvrythngServiceBase {
 
 			return this.queryParam;
 		}
+	}
+
+	public enum IdentifyType {
+		ONEDBARCODE("1dbarcode"),
+		QRCODE("qrcode"),
+		OBJPIC("objpic"),
+		DATAMATRIX("datamatrix"),
+		UNKNOWN("unknown");
+
+		private final String queryParam;
+
+		public final static String TYPE_QUERY_PARAM_KEY = "type";
+
+		IdentifyType(final String queryParam) {
+
+			this.queryParam = queryParam;
+		}
+
+		public QueryParamValue getQueryParam() {
+
+			return new QueryParamValue(TYPE_QUERY_PARAM_KEY, this.queryParam);
+		}
+
+
 	}
 
 	/**
@@ -83,6 +109,77 @@ public class ScanService extends EvrythngServiceBase {
 		UrlBinding imageInBinding = new UrlBinding();
 		imageInBinding.setImage(base64Image);
 		return recognitionsCreator(imageInBinding, methods);
+	}
+
+	/**
+	 * POST {@value #PATH_IDENTIFY}
+	 * <p>
+	 * Scan a Base64 image. Return the corresponding binding in
+	 *
+	 * @param base64Image base64 encoded image, with mime type header.
+	 * @return a preconfigured {@link Builder}
+	 */
+	public Builder<UrlBinding> identifyCreator(final String base64Image) throws EvrythngException {
+		return identifyCreator(base64Image, IdentifyType.UNKNOWN);
+	}
+
+	/**
+	 * POST {@value #PATH_IDENTIFY}
+	 * <p>
+	 * Identify a Base64 image. Return the corresponding bindings
+	 *
+	 * @param base64Image base64 encoded image, with mime type header.
+	 * @param type   one available {@link IdentifyType}
+	 * @return a preconfigured {@link Builder}
+	 */
+	public Builder<UrlBinding> identifyCreator(final String base64Image, final IdentifyType type) throws EvrythngException {
+		UrlBinding imageInBinding = new UrlBinding();
+		imageInBinding.setImage(base64Image);
+		return identifyCreator(imageInBinding, type);
+	}
+
+	/**
+	 * POST {@value #PATH_IDENTIFY}
+	 * <p>
+	 * Identify an image. Return the corresponding bindings
+	 *
+	 * @param image Image as an input stream.
+	 * @param mime mime type
+	 * @param type   one available {@link IdentifyType}
+	 * @return a preconfigured {@link Builder}
+	 */
+	public Builder<UrlBinding> identifyCreator(final InputStream image, final String mime, final IdentifyType type) throws EvrythngException, IOException {
+		String mimeAndB64 = encodeBase64(image, mime);
+		return identifyCreator(mimeAndB64, type);
+	}
+
+	/**
+	 * POST {@value #PATH_IDENTIFY}
+	 * <p>
+	 * Identify an image. Return the corresponding bindings
+	 *
+	 * @param imageInBinding image {@link UrlBinding} instance
+	 * @return a preconfigured {@link Builder}
+	 */
+	public Builder<UrlBinding> identifyCreator(final UrlBinding imageInBinding) throws EvrythngException {
+		return identifyCreator(imageInBinding, IdentifyType.UNKNOWN);
+	}
+
+	/**
+	 * POST {@value #PATH_IDENTIFY}
+	 * <p>
+	 * Identify an image. Return the corresponding bindings
+	 *
+	 * @param imageInBinding image {@link UrlBinding} instance
+	 * @param type   one available {@link IdentifyType}
+	 * @return a preconfigured {@link Builder}
+	 */
+	public Builder<UrlBinding> identifyCreator(final UrlBinding imageInBinding, final IdentifyType type) throws EvrythngException {
+		Builder<UrlBinding> result = post(PATH_IDENTIFY, imageInBinding, Status.OK, new TypeReference<UrlBinding>() {
+
+		});
+		result.queryParam(type.getQueryParam());
+		return result;
 	}
 
 	/**
