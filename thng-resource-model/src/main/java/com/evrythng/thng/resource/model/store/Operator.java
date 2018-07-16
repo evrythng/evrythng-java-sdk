00ea177922d2a937074a07a85dfbcfdeba781c18
Stephen Young <stephen.young@evrythng.com>
2018-07-16 09:44:58 +0100

    Extracting AuthProvider into its own class

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Operator.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Operator.java
@@ -9,41 +9,12 @@ import com.evrythng.commons.EnumUtils;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Map;
+import com.evrythng.thng.resource.model.store.AuthProvider;
 
 /**
  * Model representation for <em>operator</em>.
  */
 public class Operator extends AbstractUser {
-
-	public enum AuthProvider {
-		GOOGLE("google");
-
-		private final String name;
-
-		private static final Map<String, AuthProvider> names;
-
-		AuthProvider(final String name) {
-
-			this.name = name;
-		}
-
-		static {
-			names = EnumUtils.createNames(values());
-		}
-
-		@JsonValue
-		@Override
-		public String toString() {
-
-			return name;
-		}
-
-		@JsonCreator
-		public static AuthProvider fromString(final String name) {
-
-			return EnumUtils.fromString(names, name);
-		}
-	}
 
 	private static final long serialVersionUID = 2282241536928517609L;
 	private OperatorStatus operatorStatus;
