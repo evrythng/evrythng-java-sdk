2f95992e4fb4a195e0ffc9f6dba4d67fcc46b63a
Stephen Young <stephen.young@evrythng.com>
2018-09-27 15:12:06 +0100

    Adding 'equals' and 'hashCode' methods for 'Campaign'

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Campaign.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Campaign.java
@@ -6,6 +6,8 @@
 
 package com.evrythng.thng.resource.model.store;
 
+import java.util.Objects;
+
 /**
  * Representation of a campaign which is a field on the {@link Project} entity
  */
@@ -47,5 +49,26 @@ public class Campaign {
 	 */
 	public void setTemplate(final Template template) {
 		this.template = template;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		final Campaign that = (Campaign) o;
+
+		return url.equals(that.getUrl()) && template.equals(that.getTemplate());
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(url, template);
 	}
 }
