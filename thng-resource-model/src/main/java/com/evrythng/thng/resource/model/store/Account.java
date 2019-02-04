0f52e83bfe60bcc24b30a0b33172c6f39f3572b9
Aleksandr Dorogush <aleksandr.dorogush@evrythng.com>
2019-02-04 18:53:26 +0300

    AC-466: New field Account.defaultUrl.

--- thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Account.java
+++ thng-resource-model/src/main/java/com/evrythng/thng/resource/model/store/Account.java
@@ -22,6 +22,10 @@ public class Account extends DurableResourceModel {
 	 */
 	private String imageUrl;
 	private Boolean tfaRequired;
+	/**
+	 * The account's default url.
+	 */
+	private String defaultUrl;
 
 	public String getName() {
 
@@ -50,6 +54,7 @@ public class Account extends DurableResourceModel {
 		sb.append("id='").append(getId()).append("\', ");
 		sb.append("name='").append(name).append("\', ");
 		sb.append("imageUrl='").append(imageUrl).append("\'");
+		sb.append("defaultUrl='").append(defaultUrl).append("\'");
 		sb.append('}');
 		return sb.toString();
 	}
@@ -61,4 +66,19 @@ public class Account extends DurableResourceModel {
 	public void setTfaRequired(final Boolean tfaRequired) {
 		this.tfaRequired = tfaRequired;
 	}
+
+	/**
+	 * @return the defaultUrl
+	 */
+	public String getDefaultUrl() {
+		return defaultUrl;
+	}
+
+	/**
+	 * @param defaultUrl the defaultUrl to set
+	 */
+	public void setDefaultUrl(final String defaultUrl) {
+		this.defaultUrl = defaultUrl;
+	}
+
 }
